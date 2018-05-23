6973e3e481d63d2a979b2fa3826ecf4f670e215a
Christopher Kibble <christopher.kibble@biogen.com>
2018-05-23 12:22:49 -0400

    Better method to update background

--- unlocked.ps1
+++ unlocked.ps1
@@ -1,28 +1,45 @@
 Param(
-    [switch]$background   # Keep a folder of ugly background images in a subfolder called backgrounds
+    [switch]$background
 )
 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
 
-$background = $true
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+
 
 if($background) {
     $images = Get-ChildItem "$PSScriptRoot\backgrounds\*" -Include *.png,*.jpg
+    
     if(-not $images) {
         Write-Error "There are no background images in $PSScriptRoot\backgrounds"
     } else {
         $newBackground = Get-Random -InputObject $images
+        
+        Copy-Item $newBackground "$env:TEMP\UnlockedComputerBackground.bmp" -Force
+                
         Try {
-            Copy-Item $newBackground $env:TEMP -Force
-            Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name Wallpaper -Value "$($env:temp)\$($newBackground.Name)"
-        } catch {
-            Write-Error "Unable to copy background to $($env:temp)"
-        }
-
-        Try {
-            Start-Process "RunDll32.exe" -ArgumentList @("user32.dll,UpdatePerUserSystemParameters")
+            # Source/Inspiration: https://stackoverflow.com/questions/28180893/how-to-change-wall-paper-by-using-powershell
+            $SPI_SETDESKWALLPAPER = 0x0014
+            $UpdateIniFile = 0x01
+            $SendChangeEvent = 0x02
+            
+            $fWinIni = $UpdateIniFile -bor $SendChangeEvent 
+            
+            [void][Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, "$env:TEMP\UnlockedComputerBackground.bmp", $fWinIni)
             Write-Host "$($newBackground.Name) Applied"
         } Catch {
-            Write-Error "Error calling RunDll32 to Update Background"            
+            Write-Error "Error Updating Background"            
         }
     }
 }
+
